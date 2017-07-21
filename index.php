b8a56a54e2443999923c21ff321bebd04ab19ddd
Mati Halperin <matias.halperin@gmail.com>
2017-07-21 16:34:48 -0300

    Major improvements

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+
+		<?php require "include/head.html"; ?>
+
+		<link rel="stylesheet" type="text/css" href="css/navigation.css">
+		<link rel="stylesheet" type="text/css" href="css/input.css">
+		<link rel="stylesheet" type="text/css" href="css/font.css">
+	</head>
+	<body style="background-color: #fafafa;">
+		<!-- Navigation -->
+		<?php require "components/navigation.html"; ?>
+
+		<div class="container">
+			<h5>Recientes</h5>
+			<div class="row">
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<li class="divider"></li>
+			<h5>Recomendados</h5>
+			<div class="row">
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m3">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://i.ytimg.com/vi/2OgLKz9yQ0Q/hqdefault.jpg?custom=true&w=246&h=138&stc=true&jpg444=true&jpgq=90&sp=68&sigh=IE1JFDEOZl_4r872Wlo5ydYUKjc">
+						</div>
+						<div class="card-content">
+							<strong>Así es un día normal por calles de Venezuela | NO HAY PAN</strong>
+							<p>Luisito Comunica</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php require "include/scripts.html"; ?>
+	</body>
+</html>
