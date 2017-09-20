ac79b0e5698297da3f6c5b2b4df7fe2af7772f3e
Mati Halperin <matias.halperin@gmail.com>
2017-09-20 19:12:05 -0300

    Improve notifications

--- components/navigation/navigation.php
+++ components/navigation/navigation.php
@@ -21,10 +21,10 @@
                 <?php
                 if (isset($_SESSION["UserCode"])) {
                     echo '<li><i id="search-mobile-icon" class="material-icons grey-text hide-on-med-and-up" style="margin-right: 15px;">search</i></li>
-                          <li><i class="material-icons grey-text dropdown-button option" data-belowOrigin="true" data-constrainWidth="false" data-activates="Notifications">notifications</i></li>
-                          <li><img class="dropdown-button circle option" width="40px" height="40px" style="margin-right: 20px; margin-left: 15px; vertical-align: middle;" src="' . $_SESSION["Image"] . '" data-belowOrigin="true" data-constrainWidth="false" data-activates="Account"></li>
+                          <li><i class="material-icons grey-text dropdown-button option" data-belowOrigin="true" data-constrainWidth="false" data-activates="notifications">notifications</i></li>
+                          <li><img class="dropdown-button circle option" width="40px" height="40px" style="margin-right: 20px; margin-left: 15px; vertical-align: middle;" src="' . $_SESSION["Image"] . '" data-belowOrigin="true" data-constrainWidth="false" data-activates="account"></li>
 
-                          <ul id="Account" class="dropdown-content">
+                          <ul id="account" class="dropdown-content">
                               <div class="valign-wrapper grey lighten-3">
                                   <img class="circle" width="50px" height="50px" style="margin-left: 20px; margin-right: 20px;" src="' . $_SESSION["Image"] . '">
                                   <p class="black-text" style="margin-right: 20px; line-height: 25px;"><strong>' . $_SESSION["Name"] . '</strong><br>' . $_SESSION["Email"] . '</p>
@@ -33,11 +33,19 @@
                               <li><a href="/InteractED/login/logout.php" class="black-text"><i class="material-icons grey-text">exit_to_app</i>Salir</a></li>
                           </ul>
 
-                          <ul id="Notifications" class="dropdown-content" style="transform: translateY(-15px);">
+                          <ul id="notifications" class="dropdown-content notification-box">
                               <div class="valign-wrapper grey lighten-3">
-                                  <h2 class="black-text" style="font-size: 1.2rem; font-weight: 400; line-height: 1rem; margin-left: 16px;">Notificaciones</h2>
-                                  <i class="material-icons" style="margin-left: 310px; margin-right: 16px; color: #959595;">settings</i>
+                                  <h2 class="black-text notification-box-title">Notificaciones</h2>
+                                  <i class="material-icons notification-box-icon">settings</i>
                               </div>
+                              <li>
+                                  <div class="notification-wrapper">
+                                      <img class="circle notification-user" src="https://lh5.googleusercontent.com/-5nektcI4MAY/AAAAAAAAAAI/AAAAAAAAAAA/7JwddinvPqI/s96-mo/photo.jpg">
+                                      <p class="notification-text">Carga reciente recomendada: La fuerte discusión entre Toranzo y Zubeldía en Racing</p>
+                                      <img class="notification-image" src="https://i.ytimg.com/vi/PCyytKlCrIk/hqdefault.jpg">
+                                      <i class="material-icons notification-options">more_vert</i>
+                                  </div>
+                              </li>
                           </ul>';
                 }
                 else {
