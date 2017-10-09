289fac4f1e87467568fd6bdcddd1b708f1b1f70d
Sponja- <tomas.spognardi@gmail.com>
2017-10-09 07:57:16 -0300

    Fixed database

--- include/connect.php
+++ include/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "root", "InteractED");
+$conn = new mysqli("localhost", "root", "root", "database");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
