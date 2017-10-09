96564f2921b0bac827db5e467a0c93514977e92c
Sponja- <tomas.spognardi@gmail.com>
2017-10-09 10:30:30 -0300

    Fixed category system

--- category/category.js
+++ category/category.js
@@ -5,7 +5,7 @@ var category = {
     // loads the tree into memory
     loadTree: function() {
         $.getJSON("/InteractED/category/categories.json", function(data) {
-            this.tree = data;
+            category.tree = data;
         });
     },
 
@@ -15,7 +15,7 @@ var category = {
         $.ajax({
             url: "/InteractED/category/UploadCategories.php",
             type: "POST",
-            data: { categories: JSON.stringify(this.tree) } ,
+            data: { categories: JSON.stringify(category.tree) } ,
             success: function(data) {
                 console.log(data);
             },
@@ -27,41 +27,41 @@ var category = {
 
     // Returns an array with the children of a node
     getChildren: function(name) {
-        return this.tree[name].children;
+        return category.tree[name].children;
     },
 
 
     // Returns a string with the parent of a node
     getParent: function(name) {
-        return this.tree[name].parent;
+        return category.tree[name].parent;
     },
 
     // Only used in getAbsolutePath
     getAbsolutePath_helper: function(name, path_so_far) {
-        var parent = this.tree[name].parent;
+        var parent = category.tree[name].parent;
         if(parent == "root" || parent == "none")
             return path_so_far;
         path_so_far = parent + ">" + path_so_far;
-        return getAbsolutePath_helper(parent, path_so_far);
+        return category.getAbsolutePath_helper(parent, path_so_far);
     },
 
     // Returns the absolute path of a category, for example:
     // Technology>Programming>Javascript
     getAbsolutePath: function(name) {
-        return getAbsolutePath_helper(name, name);
+        return category.getAbsolutePath_helper(name, name);
     },
 
 
     // Adds a category to the category tree
     addCategory: function(name, parent_name = "root") {
-        if(!this.tree[parent_name])
+        if(!category.tree[parent_name])
             throw "Parent category doesn't exist";
-        else if(!this.tree[name]) {
-            this.tree[name] = {
+        else if(!category.tree[name]) {
+            category.tree[name] = {
                 parent: parent_name,
                 children: []
             };
-            this.tree[parent_name].children.push(name);
+            category.tree[parent_name].children.push(name);
         }
         else
             throw "Category already exists";
@@ -69,13 +69,13 @@ var category = {
 
     getCategories: function() {
         var result = [] 
-        for(var cat in this.tree)
-            if(this.tree.hasOwnProperty(cat))
+        for(var cat in category.tree)
+            if(category.tree.hasOwnProperty(cat))
                 result.push(cat);
         return result;
     },
 
     categoryExists: function(name) {
-        return this.tree.hasOwnProperty(name);
+        return category.tree.hasOwnProperty(name);
     }
 }
