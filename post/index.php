7b72621c90baabf29df9a91c98a75658af27080d
Mati Halperin <matias.halperin@gmail.com>
2017-10-19 12:23:19 -0300

    New rating system and make posts more dynamic

--- post/index.php
+++ post/index.php
@@ -1,19 +1,37 @@
-<?php session_start(); ?>
+<?php
+session_start();
+
+include "../include/connect.php";
+
+$sql = "SELECT * FROM Articles WHERE PostID = " . $_GET["id"];
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0)
+    while ($row = $result->fetch_assoc()) {
+        $Title = $row["Title"];
+    }
+else
+    header("Location: ../");
+
+$conn->close();
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Titulo</title>
+        <title><?= $Title ?> - InteractED</title>
 
         <?php require "../include/head.html"; ?>
 
         <link rel="stylesheet" href="../components/navigation/navigation.css">
+        <link rel="stylesheet" href="../components/rating/rating.css">
         <link rel="stylesheet" href="../components/comments/comments.css">
     </head>
     <body class="grey lighten-5">
         <?php require "../components/navigation/navigation.php"; ?>
 
         <div class="container">
-            <p>Hola, soy un post</p>
+            <?= file_get_contents("content/" . $_GET["id"] . "/index.html") ?>
+            <?php require "../components/rating/index.html"; ?>
             <?php require "../components/comments/index.php"; ?>
         </div>
 
