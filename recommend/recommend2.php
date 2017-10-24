070ee98a838ac9569234da074c3c7c0d38c6531f
Sponja- <tomas.spognardi@gmail.com>
2017-10-24 02:52:17 -0300

    Finished recommendation by tags and renamed a few things in the database

--- recommend/recommend2.php
+++ recommend/recommend2.php
@@ -1,14 +1,28 @@
 <?php
 require "../include/connect.php";
 
-function getTags() {
-	$sql = "SELECT * FROM Users WHERE User='" . $_SESSION["UserCode"] . "'";
-	$result = $conn->query($sql);
-	while($row = $result->fetch_assoc()) {
-		$sql = "SELECT T.TagName FROM Tags T
-				INNER JOIN Recents R ON T.PostID = R.PostID
-				"
+function postsBySimilarTags() {
+	$sql = "SELECT T.TagName FROM Tags T
+			INNER JOIN Visited V ON T.PostID = V.PostID
+			WHERE V.UserCode = " . $_SESSION["UserCode"] . " ORDER BY V.DateLastVisited DESC LIMIT 10";
+	$tag_result = $conn->query($sql);
+	if($tag_result->num_rows > 0) {
+		while($tag_row = $result->fetch_assoc()) {
+			$sql = "SELECT A.PostID FROM Tags T
+					INNER JOIN Articles A ON T.PostID = A.PostID
+					WHERE T.TagName = '" . $tag_row["TagName"] . "' AND A.PostID NOT IN(SELECT PostID FROM Visited WHERE UserCode = '" . $_SESSION["UserCode"] . "')";
+			$post_result = $conn->query($sql);
+			while($post_row = $post_result->fetch_assoc())
+				$recommendedPostIDs[] = $post_row["PostID"];
+		}
 	}
+	else {
+		// El usuario no visito ningun post
+	}
+}
+
+function postsBySimilarPeople() {
+
 }
 
 ?>
